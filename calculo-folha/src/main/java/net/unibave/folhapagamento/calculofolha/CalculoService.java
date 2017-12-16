fd1cf94b43b17e59fd51d96869120646d021eb85
Bruno <bruno.kurzawe@gmail.com>
2017-12-16 14:35:54 -0200

    teste

--- calculo-folha/src/main/java/net/unibave/folhapagamento/calculofolha/CalculoService.java
+++ calculo-folha/src/main/java/net/unibave/folhapagamento/calculofolha/CalculoService.java
@@ -7,6 +7,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import net.unibave.folhapagamento.Funcionario.Funcionario;
 import net.unibave.folhapagamento.holerite.Holerite;
+import net.unibave.folhapagamento.holerite.HoleriteRepository;
 import net.unibave.folhapagamento.holerite.HoleriteService;
 
 @Stateless
@@ -14,16 +15,27 @@ public class CalculoService {
 
     @Inject
     private HoleriteService holeriteService;
+    @Inject
+    private HoleriteRepository repository;
 
     public void calculaFolha(final CalculoFolhaDTO calculo) {
 
         //chamada remota de funcionarios 8080
         List<Funcionario> funcionarios = new ArrayList<>();
 
+        
+        
         for (Funcionario funcionario : funcionarios) {
 
-            //Calculo
-            Holerite holerite = Holerite.builder().funcionario(funcionario).dataFolha(calculo.getDataFolha()).valorIr(BigDecimal.ZERO).valorIss(BigDecimal.ZERO).valorSalarioLiquido(BigDecimal.ZERO).build();
+            //Calculo - chama método
+            Holerite holerite = Holerite.builder()
+                    .funcionario(funcionario)
+                    .dataFolha(calculo.getDataFolha())
+                    .valorIr(BigDecimal.ZERO)
+                    .valorIss(BigDecimal.ZERO)
+                    .valorSalarioLiquido(BigDecimal.ZERO)
+                    .build();
+
             holeriteService.save(holerite);
         }
     }
